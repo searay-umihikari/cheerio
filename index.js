4ac24f8b34602c38482f0e93f2364dcf434b9018
小谷野 諭 <koyano-satoshi@dmm.com>
2018-02-20 21:54:33 +0900

    add async

--- index.js
+++ index.js
@@ -3,34 +3,95 @@ const cio = require('cheerio')
 const fs = require('fs')
 const iconv = require('iconv-lite');
 
-const baseUrl='http://actress.dmm.co.jp'
-const topUrl=baseUrl+'/-/top/';
+const baseUrl = 'http://actress'
+const topUrl = baseUrl + '/-/top/';
 
-req({url:topUrl, encoding: null }, (e,res,body)=>{
 
-    if(e){
-        console(e)
-    }
+main()
 
-    try{
 
-        let buf = new Buffer(body);
-        body = iconv.decode(buf,'euc-jp');
-        const $ = cio.load(body)
+function get$(body){
+  let buf = new Buffer(body)
+  body = iconv.decode(buf, 'euc-jp')
+  const $ = cio.load(body)
+  return $;
+}
 
-        // fs.writeFileSync('out.txt',body)
 
-        $('.menu_aiueo td>a').each( function(d,elm){
+async function main() {
+  let indexULRList = []
 
-            //fs.appendFileSync('out.txt',$(elm).text())
-            console.log( $(elm).text() )
-            console.log( baseUrl+$(elm).attr('href') )
+  //get index URL List
+  let body = await doReq(topUrl)
+  let $ = get$(body)
+  $('.menu_aiueo td>a').each(function (d, elm) {
+    let url = baseUrl + $(elm).attr('href')
+    indexULRList.push(url)
+  })
+
+
+//  for(let i=0; i<indexULRList.length; i++){
+
+    //index単位で取得
+
+    body =  req({url: indexULRList[0], encoding: null}, (e, res, body) => {
+
+      $ = get$(body)
+
+      let maxPage = Number ($("td.header").eq(1).text().match( new RegExp("全(.*)ページ中") )[1])
+      console.log(maxPage)
+
+      //for(let a=0; a<$('.list').length; a++){
+        let actUrl = $('.list').eq(0).find('.pic a').attr('href')
+        req({url: actUrl, encoding: null}, (e, res, body) => {
+
+
+
+
+          $ = get$(body)
+          let link = $("link[rel=canonical]").attr('href')
+          let names = $(".t1 h1").text().match( new RegExp("(.*)（(.*)）") )
+          let name = names[1]
+          let yomi = names[2]
+          let img = $('.area-av30 td').eq('0').find("img").attr('src')
+          let birth = $('.area-av30 table tr').eq('0').find("td").eq('1').text()
+          let constellation = $('.area-av30 table tr').eq('1').find("td").eq('1').text()
+          let blood = $('.area-av30 table tr').eq('2').find("td").eq('1').text()
+          let size = $('.area-av30 table tr').eq('3').find("td").eq('1').text()
+          let birthplace = $('.area-av30 table tr').eq('4').find("td").eq('1').text()
+          let hobby = $('.area-av30 table tr').eq('5').find("td").eq('1').text()
+
+          let infoData={
+            name:name,
+            yomi:yomi,
+            url:link,
+            img:img,
+            birth:birth,
+            constellation:constellation,
+            blood:blood,
+            size:size,
+            birthplace:birthplace,
+            hobby:hobby
+          }
+          console.log(infoData)
+
         })
+      //}
+    })
 
+// }
 
-    }catch(e){
-        console.log(e)
-    }
+}
 
-
-})
+function doReq(url) {
+  return new Promise(function (resolve, reject) {
+    req({url: url, encoding: null}, (e, res, body) => {
+      if (e) {
+        reject(e)
+      } else {
+        console.log("body get")
+        resolve(body)
+      }
+    })
+  })
+}
