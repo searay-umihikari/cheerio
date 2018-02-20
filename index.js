b99412e783ca8a79f073eebcf5ca23314e438fb5
satoshi koyano <searay@satoshis-MacBook-Pro.local>
2018-02-21 01:48:23 +0900

    modify index

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const iconv = require('iconv-lite');
 const baseUrl = 'http://actress'
 const topUrl = baseUrl + '/-/top/';
 
+let getPageIndex={}
 
 main()
 
@@ -30,62 +31,95 @@ async function main() {
   })
 
 
-//  for(let i=0; i<indexULRList.length; i++){
+  for(let i=0; i<1; i++){
+  // for(let i=0; i<indexULRList.length; i++){
 
-    //index単位で取得
+      //index単位で取得
+      getUrl=indexULRList[i];
 
-    body =  req({url: indexULRList[0], encoding: null}, (e, res, body) => {
+      body = await doReq(getUrl)
 
       $ = get$(body)
-
       let maxPage = Number ($("td.header").eq(1).text().match( new RegExp("全(.*)ページ中") )[1])
-      console.log(maxPage)
 
-      //for(let a=0; a<$('.list').length; a++){
-        let actUrl = $('.list').eq(0).find('.pic a').attr('href')
-        req({url: actUrl, encoding: null}, (e, res, body) => {
+      for(let a=0; a<$('.list').length; a++){
+      // for(let a=0; a<1; a++){
+        let actUrl = $('.list').eq(a).find('.pic a').attr('href')
+        perceActPage(actUrl)
+      }
 
-
-
-
+        for(let p=2; p<=1; p++){
+        // for(let p=2; p<=maxPage; p++){
+          let pageUrl=getUrl+"page="+p
+          // body =  req({url: pageUrl, encoding: null}, (e, res, body) => {
+          body = await doReq(pageUrl)
           $ = get$(body)
-          let link = $("link[rel=canonical]").attr('href')
-          let names = $(".t1 h1").text().match( new RegExp("(.*)（(.*)）") )
-          let name = names[1]
-          let yomi = names[2]
-          let img = $('.area-av30 td').eq('0').find("img").attr('src')
-          let birth = $('.area-av30 table tr').eq('0').find("td").eq('1').text()
-          let constellation = $('.area-av30 table tr').eq('1').find("td").eq('1').text()
-          let blood = $('.area-av30 table tr').eq('2').find("td").eq('1').text()
-          let size = $('.area-av30 table tr').eq('3').find("td").eq('1').text()
-          let birthplace = $('.area-av30 table tr').eq('4').find("td").eq('1').text()
-          let hobby = $('.area-av30 table tr').eq('5').find("td").eq('1').text()
-
-          let infoData={
-            name:name,
-            yomi:yomi,
-            url:link,
-            img:img,
-            birth:birth,
-            constellation:constellation,
-            blood:blood,
-            size:size,
-            birthplace:birthplace,
-            hobby:hobby
+          for(let a=0; a<$('.list').length; a++){
+            let actUrl = $('.list').eq(a).find('.pic a').attr('href')
+            perceActPage(actUrl)
           }
-          console.log(infoData)
+       }
 
-        })
-      //}
-    })
+  }
+}
 
-// }
+function perceActPage(url){
+
+  req({url: url, encoding: null}, (e, res, body) => {
+
+    $ = get$(body)
+    let link = $("link[rel=canonical]").attr('href')
+    let dmm_id = link.match( new RegExp("actress_id=(.*)/") )[1]
+    let names = $(".t1 h1").text().match( new RegExp("(.*)（(.*)）") )
+    let name = names[1]
+    let yomi = names[2]
+    let img = $('.area-av30 td').eq('0').find("img").attr('src')
+    let birth = $('.area-av30 table tr').eq('0').find("td").eq('1').text()
+    let constellation = $('.area-av30 table tr').eq('1').find("td").eq('1').text()
+    let blood = $('.area-av30 table tr').eq('2').find("td").eq('1').text()
+    let size = $('.area-av30 table tr').eq('3').find("td").eq('1').text()
+
+    let T = (size==="----" && size.indexOf("T")===-1) ? '' : size.match( new RegExp(/T(\d+)/) )[1]
+    let B = (size==="----" && size.indexOf("B")===-1) ? '' : size.match( new RegExp(/B(\d+)/) )[1]
+    let W = (size==="----" && size.indexOf("W")===-1) ? '' : size.match( new RegExp(/W(\d+)/) )[1]
+    let H = (size==="----" && size.indexOf("H")===-1) ? '' : size.match( new RegExp(/H(\d+)/) )[1]
+
+  console.log(T)
+
+    let birthplace = $('.area-av30 table tr').eq('4').find("td").eq('1').text()
+    let hobby = $('.area-av30 table tr').eq('5').find("td").eq('1').text()
+    hobby=hobby.replace(',','、')
+
+
+    let infoData={
+      dmm_id:dmm_id,
+      name:name,
+      yomi:yomi,
+      url:link,
+      img:img,
+      birth:birth,
+      constellation:constellation,
+      blood:blood,
+      T:T,
+      B:B,
+      W:W,
+      H:H,
+      birthplace:birthplace,
+      hobby:hobby
+    }
+
+    let data = [dmm_id,name,yomi,img,birth,constellation,blood,size,birthplace,hobby].join()+'\n'
+    console.log(data);
+    fs.appendFileSync('act.csv',data)
+
+  })
 
 }
 
+
 function doReq(url) {
   return new Promise(function (resolve, reject) {
-    req({url: url, encoding: null}, (e, res, body) => {
+    req({url: url, encoding: null}, function (e, res, body) {
       if (e) {
         reject(e)
       } else {
