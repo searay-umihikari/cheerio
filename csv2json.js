3e93c2eec2086a35b48e98e11dff13b79c86a2b4
koyano-satoshi <koyano-satoshi@dmm.com>
2018-02-28 19:21:15 +0900

    fix

--- csv2json.js
+++ csv2json.js
@@ -9,9 +9,14 @@ csv().fromFile('./actress.csv').on('json',(json)=>{
 
 const fs =require('fs')
 const Converter=require('csvtojson').Converter
-var conv=new Converter({});
+var conv=new Converter({
+  toArrayString:true,
+  trim:true,
+  checkType:true,
+});
 
 conv.on('end_parsed',(jsonArray)=>{
+  console.log(jsonArray)
   fs.writeFileSync('./actress.json',JSON.stringify(jsonArray,null))
   console.log('finish writefile')
 })
